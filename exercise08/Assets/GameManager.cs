e566564e86cec6d62896d19b3a6bdc6b1cceaff7
Radha-cal <radha.hosad@gmail.com>
2023-04-05 12:36:07 -0400

    Skin picker

--- exercise08/Assets/GameManager.cs
+++ exercise08/Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -20,16 +21,39 @@ public class GameManager : MonoBehaviour
     public petPickScript selectedPet;
     public float petOption = 0;
 
+    public Slider tone;
+    public Slider undertone;
+    public Slider skinSat;
+    public Color skinColor;
+    public Material skin;
+
 
     // Start is called before the first frame update
     void Start()
     {
+         tone.maxValue = 1;
+        undertone.maxValue = .15f;
+        skinSat.maxValue = .3f;
 
+       // m_SliderValue.maxValue = 1;
+
+        tone.minValue = .1f;
+        undertone.minValue = 0;
+        skinSat.minValue = .2f;
+        //m_SliderValue.minValue = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Color st = new Color(tone.value, undertone.value, 1);
+        if(tone.value>= .7){
+            skinSat.minValue =.1f;
+        }
+        else{
+            skinSat.minValue =.2f;
+        }
+        skin.color=Color.HSVToRGB(undertone.value, skinSat.value,tone.value);
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
